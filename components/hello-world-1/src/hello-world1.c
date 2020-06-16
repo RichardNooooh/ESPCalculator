eeceb5aaea01e8397b75d40097642fd5a7f3825d
Ice <richard.jun.noh@gmail.com>
2020-06-16 16:19:54 -0500

    Initial commit
    This is a hello-world practice project I have been using for various
    tests with the ESP32.

---
+++ components/hello-world-1/src/hello-world1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void hello1()
+{
+	printf("Hello World, from hello-world-1 component directory");
+}
---
+++ components/hello-world-2/src/hello-world2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void hello2()
+{
+	printf("Hello World, from hello-world-2 component directory");
+}
