2a0d60800cbb96b9b8ce0e5999b43635f2a51867
Ice <richard.jun.noh@gmail.com>
2020-06-20 19:42:41 -0500

    Add starting GPIO setup commands
    To setup a GPIO pin, the struct "gpio_config_t" is needed. This struct
    contains various different values such as "intr_type" and "mode". For
    the purposes of this project, I think the only values needed would be:
    - Mode
    - Pin Bit Mask
    - Pull Down En
    - Pull Up En
    
    I am still not sure what pull down/up does, but I can mess with that
    later.

--- main/src/input.c
+++ main/src/input.c
@@ -1,3 +1,5 @@
+#include "driver/gpio.h"
+
 
 //TODO make list of calc buttons to corresponding GPIO pin
 
@@ -5,6 +7,12 @@
 //Listens for number presses
 void setup_gpio_configs()
 {
-
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = 1;  //this is the value that determines which gpio ports are setup
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
 }
 
